adcf994ff0d9d2980cb226879c3bdf8339a7d74d
gitmahin <nimahin25@gmail.com>
2025-04-17 05:58:10 +0600

    updated

--- apps/tutorial/src/app/[...slug]/page.tsx
+++ apps/tutorial/src/app/[...slug]/page.tsx
@@ -67,7 +67,7 @@ export default async function ContentPage({ params }: ContentPagePropsType) {
             <ProcessedContent data={mdxContent} />
             <TutoPagination />
           </div>
-          <aside className="w-[300px] h-[calc(100vh-70px)] overflow-y-auto sticky top-[70px] tuto-aside-nav">
+          <aside className="w-[300px] h-[calc(100vh-70px)] overflow-y-auto sticky top-[70px] tuto-aside-nav tuto_cont_aside">
             <ContentAsideNav />
           </aside>
         </div>
