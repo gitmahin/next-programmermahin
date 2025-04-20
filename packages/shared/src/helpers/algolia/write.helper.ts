969592a6e5780cd0e63b0e9c6ecb2c81eb535f10
gitmahin <nimahin25@gmail.com>
2025-04-21 04:16:49 +0600

    updated

--- packages/shared/src/helpers/algolia/write.helper.ts
+++ packages/shared/src/helpers/algolia/write.helper.ts
@@ -3,5 +3,6 @@ import { ALGOLIA_APPLICATION_ID } from "../common.helper";
 
 export const writeAlgolia = algoliaWrite(
   ALGOLIA_APPLICATION_ID || "",
+  // temporary
   "397658f3ee5476ffe91f63bb5447f688"
 );
