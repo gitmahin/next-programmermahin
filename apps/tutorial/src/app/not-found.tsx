650aded1222dd2e3543be1057ee287b11d475cb9
gitmahin <nimahin25@gmail.com>
2025-04-20 22:20:39 +0600

    updated

--- apps/tutorial/src/app/not-found.tsx
+++ apps/tutorial/src/app/not-found.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function NotFound() {
   return (
     <>
-      <h1>Not Found</h1>
+      <div className="fixed w-full h-screen bg-background-color_950C left-0 top-0 flex justify-center items-center z-[9999999999]">
+        <span>Not Found</span>
+      </div>
     </>
   );
 }
