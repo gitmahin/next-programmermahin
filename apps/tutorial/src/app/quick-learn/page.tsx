e3704af09c514c4a6bdafbba3f12ffbee276d0a5
gitmahin <nimahin25@gmail.com>
2025-05-24 17:31:15 +0600

    updated

--- apps/tutorial/src/app/quick-learn/page.tsx
+++ apps/tutorial/src/app/quick-learn/page.tsx
@@ -27,13 +27,14 @@ export default function QuickLearnHomePage() {
             return (
               <div
                 key={i}
-                className="w-full h-[250px] bg-background-color_925C rounded group relative"
+                className="w-full h-[250px] bg-background-color_925C rounded group relative overflow-hidden"
               >
                 
                   <div className="absolute inset-0 z-10 h-full w-full bg-transparent bg-[linear-gradient(to_right,var(--border-color-800C),transparent_1px),linear-gradient(to_bottom,var(--border-color-800C),transparent_1px)] bg-[size:24px_24px]"></div>
+                  <div className="absolute w-full h-full bg-gradient-to-br from-transparent via-background-color_900C to-background-color_925C z-10"></div>
 
               
-                <div className="px-5 pt-5">
+                <div className="px-5 pt-5 relative z-20">
                   <div className="w-[50px] h-[50px] rounded bg-background-color_900C shadow-md flex justify-center items-center">
                     <Image
                       src={"/color/html.svg"}
@@ -44,7 +45,7 @@ export default function QuickLearnHomePage() {
                     />
                   </div>
                 </div>
-                <div className="mt-8 px-5">
+                <div className="mt-8 px-5  relative z-20">
                   <h2 className="text-[18px]  font-medium two_line_ellipsis text-text-color_1">Learn HTML Basic to Advance</h2>
                   <p className="three_line_ellipsis text-read_2 mt-2 text-text-color_2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, eligendi. Atque molestiae vel quis fuga, tenetur dicta ipsa tempora maiores vitae minus, voluptates temporibus ex iste facere. Aperiam, id voluptatibus!</p>
                 </div>
