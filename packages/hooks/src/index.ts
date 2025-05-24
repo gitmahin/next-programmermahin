44e9171f6bbc1462b73ce630f0154b839920c2ba
gitmahin <nimahin25@gmail.com>
2025-05-24 08:16:49 +0600

    updated

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,8 +1,3 @@
 export * from "./useReactForm";
-<<<<<<< HEAD
-export * from "./useGetAnchors";
-export * from "./useProcessMDX";
-export * from "./useAnchorize";
 export * from "./useActiveAnchor";
-=======
->>>>>>> aa6cfee28ef9169eedc6396a478b881ef844976b
+
