c73ef2226c25478e6fc2c7db9349a35403f69642
gitmahin <nimahin25@gmail.com>
2025-05-25 11:06:24 +0600

    fixed aside quicklearn

--- apps/tutorial/src/app/quick-learn/components/quickLearnAsideNav.tsx
+++ apps/tutorial/src/app/quick-learn/components/quickLearnAsideNav.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useActiveAnchor } from "@programmer/hooks";
 import { extractAnchors, TocItem } from "@programmer/shared";
+import { LUCIDE_DEFAULT_ICON_SIZE, PMButton } from "@programmer/ui";
+import { TableOfContents, X } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 // interface NestedAnchor extends TocItem {
 //   children: TocItem[];
@@ -30,6 +32,7 @@ export const QuickLearnAsideNav = ({
   matterContent: string;
 }) => {
   const anchors = extractAnchors(matterContent);
+  const [openAside, setOpenAside] = useState<boolean>(false);
   // const nestedAnchors = nestAnchors(anchors);
   // console.log(nestedAnchors);
   const {
@@ -41,15 +44,30 @@ export const QuickLearnAsideNav = ({
   } = useActiveAnchor(matterContent);
 
   return (
-    <aside
-      className={`w-[270px] h-screen flex-shrink-0 sticky pt-[70px]  overflow-hidden top-0 tuto_cont_aside transition-all}`}
-    >
-      <div className="w-full h-full overflow-y-auto custom_scrollbar tuto_toc_main">
-        <div></div>
-        <ul className=" leading-7 relative pb-16">
-          {anchors.map((anchor, i) => {
-            return (
-              <>
+    <>
+      <PMButton
+        variant="silent"
+        onClick={() => setOpenAside(!openAside)}
+        className="fixed backdrop-blur-lg z-10 top-16 right-4 p-2 border border-border-color_800C hidden justify-center items-center toc_open_btn"
+        radius="tiny"
+      >
+        {openAside ? (
+          <X size={LUCIDE_DEFAULT_ICON_SIZE} className="text-text-color_1" />
+        ) : (
+          <TableOfContents
+            size={LUCIDE_DEFAULT_ICON_SIZE}
+            className={`text-text-svg_default_color`}
+          />
+        )}
+      </PMButton>
+      <aside
+        className={`w-[270px] h-screen flex-shrink-0 sticky pt-[70px] overflow-hidden top-0 tuto_cont_aside transition-all ${openAside && "right-[0_!important]"}`}
+      >
+        <div className="w-full h-full overflow-y-auto custom_scrollbar tuto_toc_main">
+          <div></div>
+          <ul className=" leading-7 relative pb-16">
+            {anchors.map((anchor, i) => {
+              return (
                 <Link href={`#${anchor.slug}`} key={i}>
                   <li
                     ref={(el) => {
@@ -67,15 +85,15 @@ export const QuickLearnAsideNav = ({
                     {anchor.content}
                   </li>
                 </Link>
-              </>
-            );
-          })}
-          <div
-            ref={tabRef}
-            className="absolute transition-all duration-200 h-[28px] bg-background-color_850C  top-0 rounded-tiny -z-10"
-          ></div>
-        </ul>
-      </div>
-    </aside>
+              );
+            })}
+            <div
+              ref={tabRef}
+              className="absolute transition-all duration-200 h-[28px] bg-background-color_850C  top-0 rounded-tiny -z-10"
+            ></div>
+          </ul>
+        </div>
+      </aside>
+    </>
   );
 };
