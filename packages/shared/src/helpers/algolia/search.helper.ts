b2995e65f5d0f8ce0ffe96a2af44553656057608
gitmahin <nimahin25@gmail.com>
2025-05-27 01:22:05 +0600

    updated

--- packages/shared/src/helpers/algolia/search.helper.ts
+++ packages/shared/src/helpers/algolia/search.helper.ts
@@ -4,4 +4,15 @@ import { ALGOLIA_APPLICATION_ID, ALGOLIA_SEARCH_API } from "../common.helper";
 export const searchAlgolia: LiteClient = algoliasearch(
   ALGOLIA_APPLICATION_ID || "",
   ALGOLIA_SEARCH_API || ""
+
+  // KEEP IT COMMENTED. Causing Error: algoliasearch.helper.js:1846 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'slice')
+  // {
+  //   requestsCache: createBrowserLocalStorageCache({
+  //     key: "algolia-search-cache",
+  //   }),
+  //   responsesCache: createFallbackableCache({
+  //     caches: [createMemoryCache(), createNullCache()],
+  //   }),
+  //   hostsCache: createMemoryCache(),
+  // }
 );
