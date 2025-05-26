269138c5b187a6ee1c5dd0376d7442ef21d20c20
gitmahin <nimahin25@gmail.com>
2025-05-26 13:35:18 +0600

    updated

--- apps/tutorial/src/app/sitemap.ts
+++ apps/tutorial/src/app/sitemap.ts
@@ -1,15 +1,21 @@
+import { getQuickLearnSlugs } from "@/lib/quicklearn/getQuickLearnSlugs";
 import { getTutorialsAbsolutePaths } from "@/lib/tutorials/getTutorialsAbsolutePaths";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const tutorialPaths = getTutorialsAbsolutePaths();
 
-    const tutorialPaths = getTutorialsAbsolutePaths()
+  const tutorials = tutorialPaths.map((path) => {
+    return {
+      url: `${process.env.NEXT_PUBLIC_BASE_PATH}/${path.tutoType}/${path.slug.join("/")}`,
+    };
+  });
 
-    const tutorials = tutorialPaths.map((path) => {
-        return {
-            url: `${path.tutoType}/${path.slug.join("/")}`
-        }
-    })
+  const quickLearnSlugs = getQuickLearnSlugs();
 
-    return [...tutorials]
+  const quickLearns = (await quickLearnSlugs).map((slug) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_PATH}/quick-learn/${slug}`,
+  }));
+
+  return [...tutorials, ...quickLearns];
 }
