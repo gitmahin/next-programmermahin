16ea1297f401deb958cc5ba5c4ca4a1dbb392c57
gitmahin <nimahin25@gmail.com>
2025-05-30 12:29:19 +0600

    added ISR

--- apps/web/src/app/(legals)/legals/[slug]/page.tsx
+++ apps/web/src/app/(legals)/legals/[slug]/page.tsx
@@ -4,19 +4,59 @@ import { getFileContent } from "@/lib/getFileContent";
 import { mdxToHtml } from "@programmer/shared";
 import { notFound } from "next/navigation";
 import React from "react";
+import fs from "fs";
+import { Metadata } from "next";
 
 interface LegalsPagePropsType {
   params: Promise<{ slug: string }>;
 }
 
+export async function generateStaticParams() {
+  const getSlugs = fs.readdirSync("src/content/pages", "utf-8");
+
+  const data = getSlugs.map((item) => ({
+    slug: item.replace(".mdx", ""),
+  }));
+
+  return data;
+}
+
+export async function generateMetadata({
+  params,
+}: LegalsPagePropsType): Promise<Metadata> {
+  const { slug } = await params;
+  const filePath = `src/content/pages/${slug}.mdx`;
+
+  try {
+    const { data: metaData } = getFileContent(filePath);
+
+    return {
+      title: metaData?.title || "Untitled",
+      description: metaData?.meta_desc || "Description not found",
+    };
+  } catch (error) {
+    return {
+      title: "Not Found",
+      description:
+        "Sorry, the page you're looking for doesn't exist or may have been moved.",
+    };
+  }
+}
+
 export default async function LegalsPage({ params }: LegalsPagePropsType) {
   const { slug } = await params;
-  
+
   const filePath = `src/content/pages/${slug}.mdx`;
   const { data, content } = getFileContent(filePath);
 
-  if(content == "") {
-    return notFound()
+  if (content == "") {
+    return notFound();
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    generateStaticParams().then((params) => {
+      console.log(params);
+    });
   }
 
   const MdxContent = mdxToHtml(content);
@@ -28,10 +68,10 @@ export default async function LegalsPage({ params }: LegalsPagePropsType) {
         </span>
         <h1 className="pm_page_title font-semibold mt-2">{data.title}</h1>
       </div>
-        <ArticlePageWrapper>
-          <ArticlePage>{MdxContent}</ArticlePage>
-        </ArticlePageWrapper>
-       <div className="mb-36"></div>
+      <ArticlePageWrapper>
+        <ArticlePage>{MdxContent}</ArticlePage>
+      </ArticlePageWrapper>
+      <div className="mb-36"></div>
     </MainContentWrapper>
   );
 }
