d072fa4445022250274b107d8c3dd90dfb7bbec4
gitmahin <nimahin25@gmail.com>
2025-05-30 13:49:47 +0600

    updated

---
+++ apps/web/src/components/underDevelopment.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const UnderDevelopment = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center px-5'>
+      <h1 className='text-center'>The page you are looking for is currently under development.</h1>
+    </div>
+  )
+}
+
