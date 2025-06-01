b2cba5d33b1b64e458783eedf07798ae5e271611
gitmahin <nimahin25@gmail.com>
2025-06-02 00:33:09 +0600

    updated

--- apps/web/src/app/(pages)/contact/page.tsx
+++ apps/web/src/app/(pages)/contact/page.tsx
@@ -59,57 +59,60 @@ const CONTACT_OPTIONS: ContactObjectType[] = [
 export default function ContactPage() {
   return (
     <div className="w-full h-screen fixed top-0 left-0 z-[9999999999] overflow-hidden bg-background-color_950C flex justify-center items-center">
-      <div className="w-full relative h-full flex justify-center items-center ">
+      <div className="w-full relative h-full flex justify-center items-center  ">
         <div className="absolute w-full h-full bg-gradient-to-r from-background-color_950C via-background-color_950C to-transparent z-[2]"></div>
 
-        <div className="relative z-20 max-w-[350px] w-full h-[500px]">
-          <div className="flex justify-center items-center w-full gap-3">
-            {CONTACT_OPTIONS.map((item, i) => {
-              return (
-                <label key={i} className="w-full ">
-                  <input
-                    type="radio"
-                    name="contactType"
-                    value={item.type}
-                    className="hidden peer"
-                    id={item.type}
-                  />
-                  <div className="text-text-color_2 peer-checked:bg-[var(--surface-purple-bg)] border-border-color_800C peer-checked:border-[var(--surface-purple-border)] peer-checked:text-[var(--surface-purple-fg)] hover:bg-[var(--surface-purple-bg)] w-full px-3 py-1.5 text-read_2 font-medium rounded border flex justify-center items-center hover:border-[var(--surface-purple-bg)] hover:text-[var(--surface-purple-fg)] cursor-pointer transition-colors">
-                    {item.label}
-                  </div>
-                </label>
-              );
-            })}
-          </div>
+        <div className="overflow-y-auto w-full h-full py-24 ">
+          <div className="relative z-20 max-w-[350px] w-full h-fit mx-auto ">
+            <div className="flex justify-center items-center w-full gap-3">
+              {CONTACT_OPTIONS.map((item, i) => {
+                return (
+                  <label key={i} className="w-full ">
+                    <input
+                      type="radio"
+                      name="contactType"
+                      value={item.type}
+                      className="hidden peer"
+                      id={item.type}
+                    />
+                    <div className="text-text-color_2 peer-checked:bg-[var(--surface-purple-bg)] border-border-color_800C peer-checked:border-[var(--surface-purple-border)] peer-checked:text-[var(--surface-purple-fg)] hover:bg-[var(--surface-purple-bg)] w-full px-3 py-1.5 text-read_2 font-medium rounded border flex justify-center items-center hover:border-[var(--surface-purple-bg)] hover:text-[var(--surface-purple-fg)] cursor-pointer transition-colors">
+                      {item.label}
+                    </div>
+                  </label>
+                );
+              })}
+            </div>
 
-          <div className="mt-5">
-            <p className="text-read_2 font-medium text-text-color_4">Name</p>
-            <Input
-              type="text"
-              placeholder="Jhon Doe"
-              className="text-read_2 mt-1 bg-background-color_925C py-5"
-              name="name"
-            />
-          </div>
+            <div className="mt-5">
+              <p className="text-read_2 font-medium text-text-color_4">Name</p>
+              <Input
+                type="text"
+                placeholder="Jhon Doe"
+                className="text-read_2 mt-1 bg-background-color_925C py-5"
+                name="name"
+              />
+            </div>
 
-          <div className="mt-3">
-            <p className="text-read_2 font-medium text-text-color_4">Email</p>
-            <Input
-              type="text"
-              placeholder="youremail@gmail.com"
-              className="text-read_2 mt-1 bg-background-color_925C py-5"
-              name="email"
-            />
-          </div>
+            <div className="mt-3">
+              <p className="text-read_2 font-medium text-text-color_4">Email</p>
+              <Input
+                type="text"
+                placeholder="youremail@gmail.com"
+                className="text-read_2 mt-1 bg-background-color_925C py-5"
+                name="email"
+              />
+            </div>
 
-
-          <div className="mt-3">
-            <p className="text-read_2 font-medium text-text-color_4">Message</p>
-            <Textarea
-              placeholder="youremail@gmail.com"
-              className="text-read_2 h-[100px] mt-1 bg-background-color_925C custom_scrollbar"
-              name="message"
-            />
+            <div className="mt-3">
+              <p className="text-read_2 font-medium text-text-color_4">
+                Message
+              </p>
+              <Textarea
+                placeholder="youremail@gmail.com"
+                className="text-read_2 h-[100px] mt-1 bg-background-color_925C custom_scrollbar"
+                name="message"
+              />
+            </div>
           </div>
         </div>
       </div>
